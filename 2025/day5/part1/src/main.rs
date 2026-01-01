0c458b6cdc311dd6d048fbc8c0f1a616ef66cd43
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-01 12:01:38 -0800

    day 5 part 1

---
+++ 2025/day5/part1/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let (inp1, inp2) = include_str!("../input/input.txt")
+        .split_once("\n\n")
+        .unwrap();
+    let numbers: Vec<i64> = inp2.split("\n").map(|x| x.parse::<i64>().unwrap()).collect();
+    let intervals: Vec<(i64, i64)> = inp1.split("\n").map(|x| {
+        let (l, r) = x.split_once("-").unwrap();
+        return (l.parse::<i64>().unwrap(), r.parse::<i64>().unwrap());
+    }).collect();
+    let ans = numbers.iter().filter(|&x| {
+        return intervals.iter().any(|(l, r)| l <= x && x <= r);
+    }).count();
+    println!("{ans}");
+}
