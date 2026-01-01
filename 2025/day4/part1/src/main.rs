feb1e05011a706bcf96e94553f0a82a17a658a9c
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-01 08:37:37 -0800

    cleanup

--- 2025/day4/part1/src/main.rs
+++ 2025/day4/part1/src/main.rs
@@ -14,32 +14,23 @@ fn main() {
         .lines()
         .map(|x| x.chars().collect())
         .collect();
+    let indices = grid.iter().enumerate().flat_map(
+        |(i, _)| {
+            grid[i].iter().enumerate().filter(|(j, val)| *(*val) == '@').map(move |x| (i, x.0))
+        }
+    ).collect::<Vec<(usize, usize)>>();
     let mut ans = 0;
-    for i in 0..grid.len() {
-        for j in 0..grid[i].len() {
-            if grid[i][j] == '.' {
-                continue;
+    for &(x1, y1) in indices.iter() {
+        let mut cnt = 0;
+        for &(x2, y2) in indices.iter() {
+            let dx = (x1 as i32 - x2 as i32).abs();
+            let dy = (y1 as i32 - y2 as i32).abs();
+            if -1 <= dx && dx <= 1 && -1 <= dy && dy <= 1 {
+                cnt += 1
             }
-            let mut cnt = 0;
-            for (dx, dy) in DIRS {
-                if !(dx == 0 && dy == 0) {
-                    let new_x = i as isize + dx;
-                    let new_y = j as isize + dy;
-                    if new_x < 0
-                        || new_y < 0
-                        || new_x >= grid.len() as isize
-                        || new_y >= grid[i].len() as isize
-                    {
-                        continue;
-                    }
-                    if grid[new_x as usize][new_y as usize] == '@' {
-                        cnt += 1;
-                    }
-                }
-            }
-            if cnt < 4 {
-                ans += 1;
-            }
+        }
+        if cnt <= 4 {
+            ans += 1;
         }
     }
     println!("{ans}");
