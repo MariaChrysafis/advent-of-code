6e3d09b617e2c914732cfe0eb481902073620d80
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2025-12-31 19:33:30 -0800

    a bit cleaner

--- 2025/day1/part2/src/main.rs
+++ 2025/day1/part2/src/main.rs
@@ -14,23 +14,6 @@ fn parse_line(line: &str) -> (Direction, i32) {
     (direction, delta)
 }
 
-fn reaches_zero (current_position: i32, delta: i32, direction: &Direction) -> (i32, i32) {
-    let mut new_position: i32 = current_position;
-    match direction {
-        Direction::Left => {
-            new_position -= delta;
-            if new_position > 0 {
-                return (0, new_position);
-            }
-            return (-new_position/100 + (current_position != 0) as i32, new_position);
-        }
-        Direction::Right => {
-            new_position += delta;
-            return (new_position/100 - current_position/100, new_position);
-        }
-    }
-}
-
 fn main() {
     let moves: Vec<(Direction, i32)> = include_str!("../input/input.txt")
         .lines()
@@ -39,9 +22,20 @@ fn main() {
     let mut current_position: i32 = 50;
     let mut cnt = 0;
     for (direction, delta) in moves {
-        let result = reaches_zero(current_position.rem_euclid(100i32), delta, &direction);
-        cnt += result.0;
-        current_position = result.1.rem_euclid(100);
+        let mut new_position: i32 = current_position;
+        match direction {
+            Direction::Left => {
+                new_position -= delta;
+                if new_position <= 0 {
+                    cnt += -new_position / 100 + (current_position + 99) / 100
+                }
+            }
+            Direction::Right => {
+                new_position += delta;
+                cnt += new_position / 100 - current_position / 100
+            }
+        }
+        current_position = new_position.rem_euclid(100);
     }
     println!("{cnt}");
 }
