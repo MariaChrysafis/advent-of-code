3af1c832f768fc57de68544d4466ce840d63f9ca
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-01 12:26:11 -0800

    cleanup

--- 2025/day5/part2/src/main.rs
+++ 2025/day5/part2/src/main.rs
@@ -18,18 +18,17 @@ fn union_intervals(mut vec: Vec<(i64, i64)>) -> Vec<(i64, i64)> {
     ans
 }
 fn main() {
-    let intervals: Vec<(i64, i64)> = include_str!("../input/input.txt")
-        .split_once("\n\n")
-        .unwrap()
-        .0
-        .split("\n")
-        .map(|x| {
-            let (l, r) = x.split_once("-").unwrap();
-            return (l.parse::<i64>().unwrap(), r.parse::<i64>().unwrap());
-        })
-        .collect();
-    let ans: i64 = union_intervals(intervals).iter().map(
-        |pr| pr.1 - pr.0 + 1
-    ).sum();
+    let ans: i64 = union_intervals(
+        include_str!("../input/input.txt")
+            .split_once("\n\n")
+            .unwrap()
+            .0
+            .split("\n")
+            .map(|x| {
+                let (l, r) = x.split_once("-").unwrap();
+                return (l.parse::<i64>().unwrap(), r.parse::<i64>().unwrap());
+            })
+            .collect(),
+    ).iter().map(|pr| pr.1 - pr.0 + 1).sum();
     print!("{ans}");
 }
