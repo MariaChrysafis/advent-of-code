2346e22de38cd476b8966452589389ac3a284b63
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2025-12-31 22:29:45 -0800

    cleaner

--- 2025/day3/part1/src/main.rs
+++ 2025/day3/part1/src/main.rs
@@ -1,19 +1,22 @@
 use std::cmp;
 
-fn main() {
-    let input = include_str!("../input/input.txt").split("\n");
-    let mut ans = 0;
-    for str in input {
-        let digits: Vec<u32> = str.chars().map(|c| c.to_digit(10).unwrap()).collect();
-        let mut mx = 0;
-        for i in 0..digits.len() {
-            for j in 0..digits.len() {
-                if i < j {
-                    mx = cmp::max(mx, 10 * digits[i] + digits[j]);
-                }
+fn max_joltage(str: String) -> u32 {
+    let digits: Vec<u32> = str.chars().map(|c| c.to_digit(10).unwrap()).collect();
+    let mut mx = 0;
+    for i in 0..digits.len() {
+        for j in 0..digits.len() {
+            if i < j {
+                mx = cmp::max(mx, 10 * digits[i] + digits[j]);
             }
         }
-        ans += mx;
     }
+    return mx;
+}
+
+fn main() {
+    let ans: u32 = include_str!("../input/input.txt")
+        .split("\n")
+        .map(|str| max_joltage(str.to_string()))
+        .sum::<u32>();
     println!("{ans}");
 }
