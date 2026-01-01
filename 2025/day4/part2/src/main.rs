2d44c362ba0ab3ccce86292afd625170859c8786
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-01 09:28:25 -0800

    cleanup

--- 2025/day4/part2/src/main.rs
+++ 2025/day4/part2/src/main.rs
@@ -1,13 +1,16 @@
 fn num_removable_rolls(roll_indices: Vec<(usize, usize)>) -> i32 {
-    let to_remove: Vec<&(usize, usize)> = roll_indices.iter().filter( |(x1, y1)| {
-        let num_roll_neighbors = roll_indices
-            .iter()
-            .filter(|&&(x2, y2)| {
-                (*x1 as i32 - x2 as i32).abs() <= 1 && (*y1 as i32 - y2 as i32).abs() <= 1
-            })
-            .count();
-        return num_roll_neighbors <= 4;
-    }).collect();
+    let to_remove: Vec<&(usize, usize)> = roll_indices
+        .iter()
+        .filter(|(x1, y1)| {
+            let num_roll_neighbors = roll_indices
+                .iter()
+                .filter(|&&(x2, y2)| {
+                    (*x1 as i32 - x2 as i32).abs() <= 1 && (*y1 as i32 - y2 as i32).abs() <= 1
+                })
+                .count();
+            return num_roll_neighbors <= 4;
+        })
+        .collect();
     if to_remove.len() == 0 {
         return 0;
     }
