9328103236cc6c44b15215d8ffaff76fec57f36e
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-01 12:54:32 -0800

    cleanup

--- 2025/day6/part1/src/main.rs
+++ 2025/day6/part1/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let input: Vec<Vec<&str>> = include_str!("../input/input.txt")
         .split("\n")
-        .map(|x| x.trim().split_whitespace().collect::<Vec<&str>>())
+        .map(|x| x.trim().split_whitespace().collect())
         .collect();
     let ans: i64 = (0..input[0].len())
         .map(|i| {
