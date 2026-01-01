9295e2669b818f6958b577e6a78cceff015ab30d
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2025-12-31 17:48:50 -0800

    format

--- 2025/day1/part1/src/main.rs
+++ 2025/day1/part1/src/main.rs
@@ -3,19 +3,22 @@ enum Direction {
     Right,
 }
 
-fn parse_line (line: &str) -> (Direction, i32) {
+fn parse_line(line: &str) -> (Direction, i32) {
     let (dir, value) = line.split_at(1);
     let value = value.parse::<i32>().expect("valid number");
     let direction = match dir {
         "L" => Direction::Left,
         "R" => Direction::Right,
-        _ => panic!("invalid input")
+        _ => panic!("invalid input"),
     };
     (direction, value)
 }
 
 fn main() {
-    let input: Vec<(Direction, i32)> = include_str!("../input/input.txt").lines().map(parse_line).collect();
+    let input: Vec<(Direction, i32)> = include_str!("../input/input.txt")
+        .lines()
+        .map(parse_line)
+        .collect();
     let mut current = 50;
     let mut ans = 0;
     for (dir, amount) in input {
