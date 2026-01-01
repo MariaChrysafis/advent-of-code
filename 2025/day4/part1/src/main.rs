a2cdc506e29a745557c0a06510034330a9370f54
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-01 08:46:21 -0800

    cleanup

--- 2025/day4/part1/src/main.rs
+++ 2025/day4/part1/src/main.rs
@@ -16,13 +16,13 @@ fn main() {
         .collect::<Vec<(usize, usize)>>();
     let mut ans = 0;
     for &(x1, y1) in roll_indices.iter() {
-        let cnt = roll_indices
+        let num_roll_neighbors = roll_indices
             .iter()
             .filter(|&&(x2, y2)| {
                 (x1 as i32 - x2 as i32).abs() <= 1 && (y1 as i32 - y2 as i32).abs() <= 1
             })
             .count();
-        if cnt <= 4 {
+        if num_roll_neighbors <= 4 {
             ans += 1;
         }
     }
