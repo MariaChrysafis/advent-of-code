8b7b59490aee4e93dd03efe52f92d0f6aa8d73be
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-01 12:52:50 -0800

    cleanup

--- 2025/day6/part1/src/main.rs
+++ 2025/day6/part1/src/main.rs
@@ -3,17 +3,18 @@ fn main() {
         .split("\n")
         .map(|x| x.trim().split_whitespace().collect::<Vec<&str>>())
         .collect();
-    let mut ans = 0;
-    for i in 0..input[0].len() {
-        ans += (0..input.len() - 1)
-            .map(|j| input[j][i].parse::<i64>().unwrap())
-            .into_iter()
-            .reduce(match input[input.len() - 1][i] {
-                "*" => |x, y| x * y,
-                "+" => |x, y| x + y,
-                _ => panic!("operator not recognized"),
-            })
-            .unwrap();
-    }
+    let ans: i64 = (0..input[0].len())
+        .map(|i| {
+            (0..input.len() - 1)
+                .map(|j| input[j][i].parse::<i64>().unwrap())
+                .into_iter()
+                .reduce(match input[input.len() - 1][i] {
+                    "*" => |x, y| x * y,
+                    "+" => |x, y| x + y,
+                    _ => panic!("operator not recognized"),
+                })
+                .unwrap()
+        })
+        .sum();
     println!("{ans}");
 }
