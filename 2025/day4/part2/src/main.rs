6a88aa5b2e4043f7cc650affa847d40f0c2d40ef
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-01 09:32:28 -0800

    cleanup

--- 2025/day4/part2/src/main.rs
+++ 2025/day4/part2/src/main.rs
@@ -1,11 +1,12 @@
-fn num_removable_rolls(roll_indices: Vec<(usize, usize)>) -> i32 {
-    let to_remove: Vec<&(usize, usize)> = roll_indices
+fn num_removable_rolls(mut roll_indices: Vec<(usize, usize)>) -> i32 {
+    let to_remove: Vec<(usize, usize)> = roll_indices
         .iter()
-        .filter(|(x1, y1)| {
+        .copied()
+        .filter(|&(x1, y1)| {
             let num_roll_neighbors = roll_indices
                 .iter()
                 .filter(|&&(x2, y2)| {
-                    (*x1 as i32 - x2 as i32).abs() <= 1 && (*y1 as i32 - y2 as i32).abs() <= 1
+                    (x1 as i32 - x2 as i32).abs() <= 1 && (y1 as i32 - y2 as i32).abs() <= 1
                 })
                 .count();
             return num_roll_neighbors <= 4;
@@ -14,9 +15,8 @@ fn num_removable_rolls(roll_indices: Vec<(usize, usize)>) -> i32 {
     if to_remove.len() == 0 {
         return 0;
     }
-    let mut next_input = roll_indices.clone();
-    next_input.retain(|x| !to_remove.contains(&x));
-    return num_removable_rolls(next_input) + to_remove.len() as i32;
+    roll_indices.retain(|x| !to_remove.contains(&x));
+    return num_removable_rolls(roll_indices) + to_remove.len() as i32;
 }
 fn main() {
     let roll_indices = include_str!("../input/input.txt")
@@ -25,8 +25,8 @@ fn main() {
         .flat_map(|(i, str)| {
             str.chars()
                 .enumerate()
-                .filter(|(j, val)| *val == '@')
-                .map(move |x| (i, x.0))
+                .filter(|(_, val)| *val == '@')
+                .map(move |(j, _): (usize, char)| (i, j))
         })
         .collect::<Vec<(usize, usize)>>();
     let ans = num_removable_rolls(roll_indices);
