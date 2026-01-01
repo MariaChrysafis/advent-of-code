5d8938bab40bcbd7d875c8e7c6d58efc2bc0094f
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-01 08:18:35 -0800

    cleanup

--- 2025/day4/part1/src/main.rs
+++ 2025/day4/part1/src/main.rs
@@ -1,7 +1,19 @@
+const DIRS: [(isize, isize); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
 fn main() {
     let grid: Vec<Vec<char>> = include_str!("../input/input.txt")
         .lines()
-        .map(|x| x.chars().collect()).collect();
+        .map(|x| x.chars().collect())
+        .collect();
     let mut ans = 0;
     for i in 0..grid.len() {
         for j in 0..grid[i].len() {
@@ -9,21 +21,19 @@ fn main() {
                 continue;
             }
             let mut cnt = 0;
-            for dx in -1i32..=1 {
-                for dy in -1i32..=1 {
-                    if !(dx == 0 && dy == 0) {
-                        let new_x = i as i32 + dx;
-                        let new_y = j as i32 + dy;
-                        if new_x < 0
-                            || new_y < 0
-                            || new_x >= grid.len() as i32
-                            || new_y >= grid[i].len() as i32
-                        {
-                            continue;
-                        }
-                        if grid[new_x as usize][new_y as usize] == '@' {
-                            cnt += 1;
-                        }
+            for (dx, dy) in DIRS {
+                if !(dx == 0 && dy == 0) {
+                    let new_x = i as isize + dx;
+                    let new_y = j as isize + dy;
+                    if new_x < 0
+                        || new_y < 0
+                        || new_x >= grid.len() as isize
+                        || new_y >= grid[i].len() as isize
+                    {
+                        continue;
+                    }
+                    if grid[new_x as usize][new_y as usize] == '@' {
+                        cnt += 1;
                     }
                 }
             }
