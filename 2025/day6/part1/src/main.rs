9fa1c347644b258ccbc797a709670a818074213d
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-01 12:52:03 -0800

    cleanup

--- 2025/day6/part1/src/main.rs
+++ 2025/day6/part1/src/main.rs
@@ -1,17 +1,19 @@
 fn main() {
     let input: Vec<Vec<&str>> = include_str!("../input/input.txt")
         .split("\n")
-        .map(|x| x.trim().split_whitespace().collect::<Vec<&str>>()).collect();
+        .map(|x| x.trim().split_whitespace().collect::<Vec<&str>>())
+        .collect();
     let mut ans = 0;
     for i in 0..input[0].len() {
-        let op = input[input.len() - 1][i];
-        let operator: fn(i64, i64) -> i64 = match op {
-            "*" => |x, y| x * y,
-            "+" => |x, y| x + y,
-            _ => panic!("operator not recognized"),
-        };
-        let result = (0..input.len()-1).map(|j| input[j][i].parse::<i64>().unwrap()).into_iter().reduce(operator);
-        ans += result.unwrap();
+        ans += (0..input.len() - 1)
+            .map(|j| input[j][i].parse::<i64>().unwrap())
+            .into_iter()
+            .reduce(match input[input.len() - 1][i] {
+                "*" => |x, y| x * y,
+                "+" => |x, y| x + y,
+                _ => panic!("operator not recognized"),
+            })
+            .unwrap();
     }
     println!("{ans}");
 }
