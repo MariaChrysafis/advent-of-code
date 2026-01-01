5ed6e1d07fe1cf473d4324d202cbbac3e0ca46bd
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2025-12-31 23:18:55 -0800

    cleanup

--- 2025/day3/part2/src/main.rs
+++ 2025/day3/part2/src/main.rs
@@ -3,8 +3,8 @@ use std::cmp;
 fn max_joltage(str: &str, batteries: usize) -> i64 {
     let digits: Vec<u32> = str.chars().map(|x| x.to_digit(10).unwrap()).collect();
     let mut dp: Vec<Vec<i64>> = vec![vec![0; batteries + 1]; str.len() + 1];
-    for ind in 1..str.len() + 1 {
-        for battery in 1..(batteries + 1) as usize {
+    for ind in 1..=str.len() {
+        for battery in 1..=batteries {
             dp[ind][battery] = cmp::max(
                 dp[ind - 1][battery].clone(),
                 10 * dp[ind - 1][battery - 1] + digits[ind - 1] as i64,
