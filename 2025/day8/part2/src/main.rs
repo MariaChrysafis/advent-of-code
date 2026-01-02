db54a34525c44b8e57654b53f5bf3c566e7107f8
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-02 09:52:08 -0800

    cleanup

---
+++ 2025/day8/part2/src/main.rs
@@ -0,0 +1,54 @@
+use std::mem::swap;
+struct Graph {
+    parent: Vec<usize>,
+    sz: Vec<usize>,
+}
+impl Graph {
+    pub fn find_head(&self, u: usize) -> usize {
+        if u == self.parent[u] {
+            u
+        } else {
+            self.find_head(self.parent[u])
+        }
+    }
+    pub fn join(&mut self, mut u: usize, mut v: usize) {
+        u = self.find_head(u);
+        v = self.find_head(v);
+        if u == v {
+            return;
+        }
+        if self.sz[u] > self.sz[v] {
+            swap(&mut u, &mut v);
+        }
+        self.parent[u] = v;
+        self.sz[v] += self.sz[u];
+    }
+    pub fn new(n: usize) -> Graph {
+        Graph {
+            parent: (0..n).collect(),
+            sz: vec![1; n],
+        }
+    }
+}
+fn distance(pos1: &[i64], pos2: &[i64]) -> i64 {
+    pos1.iter().zip(pos2).map(|(a, b)| (a - b).pow(2)).sum()
+}
+fn main() {
+    let positions: Vec<Vec<i64>> = include_str!("../input/input.txt")
+        .lines()
+        .map(|x| x.split(',').map(|x| x.parse::<i64>().unwrap()).collect())
+        .collect();
+    let mut edges: Vec<(usize, usize)> = (0..positions.len())
+        .flat_map(|i| (i + 1..positions.len()).map(move |j| (i, j)))
+        .collect();
+    edges.sort_by_key(|(i, j)| distance(&positions[*i], &positions[*j]));
+    let mut graph = Graph::new(positions.len());
+    for (i, j) in edges {
+        graph.join(i, j);
+        if graph.sz[graph.find_head(i)] == positions.len() {
+            let ans = positions[i][0] * positions[j][0];
+            print!("{ans}");
+            return;
+        }
+    }
+}
