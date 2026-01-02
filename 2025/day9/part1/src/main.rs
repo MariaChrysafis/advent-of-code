19823dc46c7773f0f31d2fc23123c53307a02155
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-02 10:30:28 -0800

    cleanup

--- 2025/day9/part1/src/main.rs
+++ 2025/day9/part1/src/main.rs
@@ -4,12 +4,12 @@ fn main() {
         .map(|x| x.split_once(",").unwrap())
         .map(|x| (x.0.parse::<i64>().unwrap(), x.1.parse::<i64>().unwrap()))
         .collect();
-    let ans: i64 = positions
+    let ans: u64 = positions
         .iter()
         .enumerate()
         .flat_map(|(i, coord1)| {
             positions[i + 1..].iter().map(move |coord2| {
-                ((coord1.0 - coord2.0).abs() + 1) * ((coord2.1 - coord1.1).abs() + 1)
+                (coord1.0.abs_diff(coord2.0) + 1) * (coord1.1.abs_diff(coord2.1) + 1)
             })
         })
         .max()
