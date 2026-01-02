b1aa26261c9b8913bfa66915342676bb25a59b95
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-01 18:56:41 -0800

    move

---
+++ 2025/day7/part1/src/main.rs
@@ -0,0 +1,28 @@
+use std::collections::HashSet;
+fn main() {
+    let mut input = include_str!("../input/input.txt")
+        .lines()
+        .map(|x| x.chars().collect::<Vec<char>>());
+    let mut beams: HashSet<i32> = HashSet::from([input
+        .next()
+        .unwrap()
+        .iter()
+        .position(|x| *x == 'S')
+        .unwrap() as i32]);
+    let mut ans = 0;
+    for row in input {
+        beams = beams
+            .iter()
+            .flat_map(|j| {
+                if row[*j as usize] != '^' {
+                    vec![*j]
+                } else {
+                    ans += 1;
+                    vec![j - 1, j + 1]
+                }
+            })
+            .filter(|&x| x >= 0 && x < row.len() as i32)
+            .collect();
+    }
+    println!("{ans}")
+}
