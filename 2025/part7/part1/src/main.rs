e16dc5c3ad93d2253f9b8b5e72d9b46679661f89
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-01 18:26:17 -0800

    day 7 part 1

---
+++ 2025/part7/part1/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let mut input = include_str!("../input/input.txt")
+        .split("\n")
+        .map(|x| x.chars().collect::<Vec<char>>());
+    let mut beams: Vec<i32> = vec![
+        input
+            .next()
+            .unwrap()
+            .iter()
+            .position(|x| *x == 'S')
+            .unwrap() as i32,
+    ];
+    let mut ans = 0;
+    for row in input {
+        let mut new_beams: Vec<i32> = beams
+            .iter()
+            .map(|j| {
+                if row[*j as usize] != '^' {
+                    return vec![*j];
+                } else {
+                    ans += 1;
+                    return vec![j - 1, j + 1];
+                }
+            })
+            .collect::<Vec<Vec<i32>>>()
+            .concat()
+            .iter()
+            .filter(|&x| *x >= 0 && *x < row.len() as i32)
+            .copied()
+            .collect();
+        new_beams.sort_unstable();
+        new_beams.dedup();
+        beams = new_beams;
+    }
+    // let ans = beams.len();
+    println!("{ans}")
+}
