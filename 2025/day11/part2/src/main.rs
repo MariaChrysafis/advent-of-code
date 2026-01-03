e8f77ce8d2b12ba50ab49e4b05ae5e83518753ba
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-02 23:35:21 -0800

    cleanup

--- 2025/day11/part2/src/main.rs
+++ 2025/day11/part2/src/main.rs
@@ -1,14 +1,15 @@
 use std::collections::HashMap;
-type Node = (String, [bool; 2]);
 
 const TARGETS: [&str; 2] = ["dac", "fft"];
+type Node = (String, [bool; 2]);
+type Adj = HashMap<String, Vec<String>>;
 
-fn dfs(adj: &HashMap<String, Vec<String>>, node: &Node, dp: &mut HashMap<Node, i64>) -> i64 {
-    if let Some(&v) = dp.get(node) {
+fn dfs(adj: &Adj, (name, flags): &Node, dp: &mut HashMap<Node, i64>) -> i64 {
+    if let Some(&v) = dp.get(&(name.clone(), *flags)) {
         return v;
     }
-    let result: i64 = adj
-        .get(&node.0)
+    let result = adj
+        .get(name)
         .into_iter()
         .flatten()
         .map(|n| {
@@ -16,37 +17,25 @@ fn dfs(adj: &HashMap<String, Vec<String>>, node: &Node, dp: &mut HashMap<Node, i
                 adj,
                 &(
                     n.clone(),
-                    std::array::from_fn(|i: usize| node.1[i] || n == TARGETS[i]),
+                    std::array::from_fn(|i| flags[i] || n == TARGETS[i]),
                 ),
                 dp,
             )
         })
         .sum();
-    dp.insert(node.clone(), result);
+    dp.insert((name.clone(), *flags), result);
     result
 }
 
 fn main() {
-    let adj: HashMap<String, Vec<String>> = include_str!("../input/input.txt")
+    let adj: Adj = include_str!("../input/input.txt")
         .lines()
-        .map(|line| {
-            let (node, neighbors) = line.split_once(':').unwrap();
-            (
-                node.to_string(),
-                neighbors
-                    .split_whitespace()
-                    .map(|x| x.to_string())
-                    .collect(),
-            )
-        })
+        .filter_map(|l| l.split_once(':'))
+        .map(|(n, nb)| (n.into(), nb.split_whitespace().map(Into::into).collect()))
         .collect();
-    let end = ("out".to_string(), [true, true]);
+    let end = ("out".into(), [true; 2]);
     println!(
         "{}",
-        dfs(
-            &adj,
-            &("svr".to_string(), [false, false]),
-            &mut HashMap::from([(end, 1)])
-        )
-    )
+        dfs(&adj, &("svr".into(), [false; 2]), &mut [(end, 1)].into())
+    );
 }
