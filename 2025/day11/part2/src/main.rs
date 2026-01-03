b6ecf5e4395ab6fd2a41beb06e8901620fc6239d
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-02 23:33:35 -0800

    cleanup

--- 2025/day11/part2/src/main.rs
+++ 2025/day11/part2/src/main.rs
@@ -9,15 +9,17 @@ fn dfs(adj: &HashMap<String, Vec<String>>, node: &Node, dp: &mut HashMap<Node, i
     }
     let result: i64 = adj
         .get(&node.0)
-        .map(|v| v.as_slice())
-        .unwrap_or(&[])
-        .iter()
+        .into_iter()
+        .flatten()
         .map(|n| {
-            let next = (
-                n.clone(),
-                [node.1[0] || n == TARGETS[0], node.1[1] || n == TARGETS[1]],
-            );
-            dfs(adj, &next, dp)
+            dfs(
+                adj,
+                &(
+                    n.clone(),
+                    [node.1[0] || n == TARGETS[0], node.1[1] || n == TARGETS[1]],
+                ),
+                dp,
+            )
         })
         .sum();
     dp.insert(node.clone(), result);
@@ -38,10 +40,13 @@ fn main() {
             )
         })
         .collect();
-    let start = &("svr".to_string(), [false, false]);
-    let end = &("out".to_string(), [true, true]);
+    let end = ("out".to_string(), [true, true]);
     println!(
         "{}",
-        dfs(&adj, start, &mut HashMap::from([(end.clone(), 1i64)]))
+        dfs(
+            &adj,
+            &("svr".to_string(), [false, false]),
+            &mut HashMap::from([(end, 1)])
+        )
     )
 }
