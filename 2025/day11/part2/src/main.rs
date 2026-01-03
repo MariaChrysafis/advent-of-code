b894fe7d07ddfc487c5400df8d8356f23e7599ce
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-02 23:32:55 -0800

    cleanup

--- 2025/day11/part2/src/main.rs
+++ 2025/day11/part2/src/main.rs
@@ -1,35 +1,34 @@
 use std::collections::HashMap;
 type Node = (String, [bool; 2]);
-struct Graph {
-    adj: HashMap<String, Vec<String>>,
-}
-impl Graph {
-    pub fn dfs(&self, node: &Node, dp: &mut HashMap<Node, i64>) -> i64 {
-        if dp.contains_key(node) {
-            return *dp.get(node).unwrap();
-        }
-        let result: i64 = self
-            .adj
-            .get(&node.0)
-            .map_or(&vec![], |v| v)
-            .iter()
-            .map(|n| {
-                let next = (
-                    n.clone(),
-                    [node.1[0] || n == "dac", node.1[1] || n == "fft"],
-                );
-                self.dfs(&next, dp)
-            })
-            .sum();
-        dp.insert(node.clone(), result);
-        result
+
+const TARGETS: [&str; 2] = ["dac", "fft"];
+
+fn dfs(adj: &HashMap<String, Vec<String>>, node: &Node, dp: &mut HashMap<Node, i64>) -> i64 {
+    if let Some(&v) = dp.get(node) {
+        return v;
     }
+    let result: i64 = adj
+        .get(&node.0)
+        .map(|v| v.as_slice())
+        .unwrap_or(&[])
+        .iter()
+        .map(|n| {
+            let next = (
+                n.clone(),
+                [node.1[0] || n == TARGETS[0], node.1[1] || n == TARGETS[1]],
+            );
+            dfs(adj, &next, dp)
+        })
+        .sum();
+    dp.insert(node.clone(), result);
+    result
 }
+
 fn main() {
     let adj: HashMap<String, Vec<String>> = include_str!("../input/input.txt")
         .lines()
-        .map(|str| {
-            let (node, neighbors) = str.split_once(':').unwrap();
+        .map(|line| {
+            let (node, neighbors) = line.split_once(':').unwrap();
             (
                 node.to_string(),
                 neighbors
@@ -39,11 +38,10 @@ fn main() {
             )
         })
         .collect();
-    let graph = Graph { adj };
     let start = &("svr".to_string(), [false, false]);
     let end = &("out".to_string(), [true, true]);
     println!(
         "{}",
-        graph.dfs(start, &mut HashMap::from([(end.clone(), 1i64)]))
+        dfs(&adj, start, &mut HashMap::from([(end.clone(), 1i64)]))
     )
 }
