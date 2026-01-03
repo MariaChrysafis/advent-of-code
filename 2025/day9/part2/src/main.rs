f23a62b41ec261bd38be838d40b893f2eb4e8d0d
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-02 20:24:54 -0800

    day 9 part 2

---
+++ 2025/day9/part2/src/main.rs
@@ -0,0 +1,90 @@
+use std::cmp;
+#[derive(PartialEq, Clone, Copy)]
+struct Point {
+    x: i64,
+    y: i64,
+}
+fn valid_pairs(input: Vec<Point>) -> Vec<(Point, Point)> {
+    let sz: usize = input.iter().map(|p| cmp::max(p.x, p.y) + 1).max().unwrap() as usize;
+    let lines: Vec<(Point, Point)> = (0..input.len())
+        .map(|i| (input[i], (input[(i + 1) % input.len()])))
+        .collect();
+    let mut is_inside = vec![vec![false; sz]; sz];
+    for (i, row) in is_inside.iter_mut().enumerate().take(sz) {
+        for (j, interior) in row.iter_mut().enumerate().take(sz){
+            let mut cntr = 0;
+            for line in lines.iter() {
+                if line.0.x == line.1.x
+                    && i < line.0.x as usize
+                    && j > cmp::min(line.0.y, line.1.y) as usize
+                    && j <= cmp::max(line.0.y, line.1.y) as usize
+                {
+                    cntr += 1;
+                }
+            }
+            *interior = cntr % 2 == 1;
+        }
+    }
+    for line in lines {
+        for i in cmp::min(line.0.x, line.1.x)..=cmp::max(line.0.x, line.1.x) {
+            for j in cmp::min(line.0.y, line.1.y)..=cmp::max(line.1.y, line.1.y) {
+                is_inside[i as usize][j as usize] = true;
+            }
+        }
+    }
+    let mut ans = vec![];
+    for &point1 in &input {
+        assert!(is_inside[point1.x as usize][point1.y as usize]);
+        for &point2 in &input {
+            let mut fine = true;
+            for i in cmp::min(point1.x, point2.x)..=cmp::max(point1.x, point2.x) {
+                for j in cmp::min(point1.y, point2.y)..=cmp::max(point1.y, point2.y) {
+                    if !is_inside[i as usize][j as usize] {
+                        fine = false;
+                        break;
+                    }
+                }
+                if !fine {
+                    break;
+                }
+            }
+            if fine {
+                ans.push((point1, point2));
+            }
+            // does this make a square??
+        }
+    }
+    ans
+}
+fn main() {
+    // tests();
+    let points: Vec<Point> = include_str!("../input/input.txt")
+        .lines()
+        .map(|x| x.split_once(",").unwrap())
+        .map(|x| (x.0.parse::<i64>().unwrap(), x.1.parse::<i64>().unwrap()))
+        .map(|(x, y)| Point { x, y })
+        .collect();
+    // coordinate compression (map a large coordinate to a smaller one)
+    let mut positions: Vec<i64> = points
+        .iter()
+        .flat_map(|point| vec![point.x, point.y])
+        .collect();
+    positions.sort();
+    positions.dedup();
+    let result = valid_pairs(
+        points
+            .iter()
+            .map(|point| Point {
+                x: positions.iter().position(|&val| val == point.x).unwrap() as i64 * 2,
+                y: positions.iter().position(|&val| val == point.y).unwrap() as i64 * 2,
+            })
+            .collect(),
+    );
+    let mut ans = 0;
+    for (point1, point2) in result {
+        let dx = positions[(point1.x as usize) / 2].abs_diff(positions[(point2.x as usize) / 2]);
+        let dy = positions[(point1.y as usize) / 2].abs_diff(positions[(point2.y as usize) / 2]);
+        ans = cmp::max(ans, (dx + 1) * (dy + 1));
+    }
+    println!("{}", ans);
+}
