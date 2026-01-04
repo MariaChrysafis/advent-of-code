f17475283fa3d049bf6a75f915de33180cc87a4e
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-04 13:08:01 -0800

    cleaup

--- 2023/day25/part1/src/main.rs
+++ 2023/day25/part1/src/main.rs
@@ -1,17 +1,15 @@
 use petgraph::graphmap::UnGraphMap;
 use rustworkx_core::connectivity::stoer_wagner_min_cut;
 fn main() {
-    let edges: Vec<(&str, &str)> = include_str!("../input/input.txt")
+    let graph: UnGraphMap<&str, ()> = include_str!("../input/input.txt")
         .split("\n")
         .flat_map(|str| {
             let (node, neighbors) = str.split_once(":").unwrap();
             neighbors
                 .split_whitespace()
-                .map(|x| (node, x))
-                .collect::<Vec<(&str, &str)>>()
+                .map(move |x| (node, x))
         })
         .collect();
-    let graph: UnGraphMap<&str, ()> = UnGraphMap::from_edges(edges);
     let min_cut = stoer_wagner_min_cut(&graph, |_| Ok::<i32, ()>(1));
     let (_, vec) = min_cut.unwrap().unwrap();
     print!("{}", vec.len() * (graph.node_count() - vec.len()));
