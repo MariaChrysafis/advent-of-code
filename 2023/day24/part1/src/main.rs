3f5818f25a39984deb4eaf50407e55b8089dde71
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-04 16:54:42 -0800

    cleanup

--- 2023/day24/part1/src/main.rs
+++ 2023/day24/part1/src/main.rs
@@ -1,9 +1,10 @@
 use geo::line_intersection::{LineIntersection, line_intersection};
 use geo::{Coord, Intersects, Line, Rect};
+use itertools::Itertools;
 const INFINITY: f64 = 100000000000000000.0;
 fn main() {
     // parse the input
-    let input: Vec<Line<f64>> = include_str!("../input/input.txt")
+    let input= include_str!("../input/input.txt")
         .lines()
         .map(|line| {
             let (pos, dir) = line.split_once("@").unwrap();
@@ -20,27 +21,18 @@ fn main() {
                 },
             }
         })
-        .collect();
+        .collect::<Vec<_>>();
     const MIN: f64 = 200000000000000.0;
     const MAX: f64 = 400000000000000.0;
     let bounds = Rect::new(Coord { x: MIN, y: MIN }, Coord { x: MAX, y: MAX });
     let count = input
         .iter()
-        .enumerate()
-        .map(|(i, segment1)| {
-            input[i + 1..]
-                .iter()
-                .filter(|segment2| match line_intersection(*segment1, **segment2) {
-                    Some(LineIntersection::SinglePoint { intersection, .. }) => {
-                        bounds.intersects(&intersection)
-                    }
-                    Some(LineIntersection::Collinear { intersection }) => {
-                        bounds.intersects(&intersection)
-                    }
-                    None => false,
-                })
-                .count()
+        .combinations(2)
+        .filter(|pair| match line_intersection(*pair[0], *pair[1]) {
+            Some(LineIntersection::SinglePoint { intersection, .. }) => bounds.intersects(&intersection),
+            Some(LineIntersection::Collinear { intersection }) => bounds.intersects(&intersection),
+            None => false,
         })
-        .sum::<usize>();
+        .count();
     println!("{}", count);
 }
