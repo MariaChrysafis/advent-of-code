9d5473572354e1325702df7bf42de79bb1313fd4
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-04 16:52:32 -0800

    cleanup

--- 2023/day24/part1/src/main.rs
+++ 2023/day24/part1/src/main.rs
@@ -1,6 +1,6 @@
 use geo::line_intersection::{LineIntersection, line_intersection};
 use geo::{Coord, Intersects, Line, Rect};
-
+const INFINITY: f64 = 100000000000000000.0;
 fn main() {
     // parse the input
     let input: Vec<Line<f64>> = include_str!("../input/input.txt")
@@ -15,8 +15,8 @@ fn main() {
                     y: pos_parts[1],
                 },
                 end: Coord {
-                    x: pos_parts[0] + dir_parts[0] * 100000000000000000.0,
-                    y: pos_parts[1] + dir_parts[1] * 100000000000000000.0,
+                    x: pos_parts[0] + dir_parts[0] * INFINITY,
+                    y: pos_parts[1] + dir_parts[1] * INFINITY,
                 },
             }
         })
@@ -24,22 +24,23 @@ fn main() {
     const MIN: f64 = 200000000000000.0;
     const MAX: f64 = 400000000000000.0;
     let bounds = Rect::new(Coord { x: MIN, y: MIN }, Coord { x: MAX, y: MAX });
-    let mut count = 0;
-    for (i, segment1) in input.iter().enumerate() {
-        for segment2 in input[i + 1..].iter() {
-            let in_bounds = match line_intersection(*segment1, *segment2) {
-                Some(LineIntersection::SinglePoint { intersection, .. }) => {
-                    bounds.intersects(&intersection)
-                }
-                Some(LineIntersection::Collinear { intersection }) => {
-                    bounds.intersects(&intersection)
-                }
-                None => false,
-            };
-            if in_bounds {
-                count += 1;
-            }
-        }
-    }
+    let count = input
+        .iter()
+        .enumerate()
+        .map(|(i, segment1)| {
+            input[i + 1..]
+                .iter()
+                .filter(|segment2| match line_intersection(*segment1, **segment2) {
+                    Some(LineIntersection::SinglePoint { intersection, .. }) => {
+                        bounds.intersects(&intersection)
+                    }
+                    Some(LineIntersection::Collinear { intersection }) => {
+                        bounds.intersects(&intersection)
+                    }
+                    None => false,
+                })
+                .count()
+        })
+        .sum::<usize>();
     println!("{}", count);
 }
