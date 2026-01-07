8eb2ac5cec92f3863ae1404db0153d35ce928c52
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-06 23:49:08 -0800

    Fix

--- 2023/day23/part1/src/main.rs
+++ 2023/day23/part1/src/main.rs
@@ -1,5 +1,3 @@
-use core::panic;
-
 #[derive(Copy, Clone, PartialEq, Eq)]
 struct Point {
     y: i32,
@@ -9,20 +7,30 @@ struct Grid {
     grid: Vec<Vec<char>>,
 }
 impl Grid {
-    fn get(&self, point: Point) -> char {
-        return self.grid[point.x as usize][point.y as usize];
+    fn get(&self, point: Point) -> Option<char> {
+        if point.x < 0
+            || point.y < 0
+            || point.x >= self.grid.len() as i32
+            || point.y >= self.grid[0].len() as i32
+        {
+            return None;
+        }
+        return Some(self.grid[point.x as usize][point.y as usize]);
     }
-    fn start(&self) -> Point {
-        return Point { x: 0, y: 0 };
-    }
-    fn end(&self) -> Point {
-        return Point {
-            x: (self.grid.len() - 1) as i32,
-            y: (self.grid[0].len() - 1) as i32,
-        };
+    fn all_points(&self) -> Vec<Point> {
+        return (0..self.grid.len())
+            .flat_map(|x| {
+                (0..self.grid[0].len())
+                    .map(|y| Point {
+                        x: x as i32,
+                        y: y as i32,
+                    })
+                    .collect::<Vec<Point>>()
+            })
+            .collect();
     }
     fn neighbors(&self, point: Point) -> Vec<Point> {
-        let ans = match self.get(point) {
+        match self.get(point).unwrap() {
             '>' => vec![Point {
                 x: point.x,
                 y: point.y + 1,
@@ -32,44 +40,39 @@ impl Grid {
                 y: point.y - 1,
             }],
             'v' => vec![Point {
-                x: point.x,
-                y: point.y + 1,
+                x: point.x + 1,
+                y: point.y,
             }],
             '^' => vec![Point {
-                x: point.x,
-                y: point.y - 1,
+                x: point.x - 1,
+                y: point.y,
             }],
-            '.' => (-1..=1)
-                .flat_map(|dx| {
-                    (-1..=1)
-                        .map(|dy| Point {
-                            x: point.x + dx,
-                            y: point.y + dy,
-                        })
-                        .collect::<Vec<Point>>()
+            '.' => [[-1, 0], [1, 0], [0, 1], [0, -1]]
+                .map(|delta| Point {
+                    x: point.x + delta[0],
+                    y: point.y + delta[1],
                 })
+                .into_iter()
                 .collect(),
             _ => panic!(),
-        };
-        ans.into_iter()
-            .filter(|&point| self.get(point) != '#')
-            .collect()
+        }
     }
 }
 fn dfs(grid: &Grid, coord: Point, past_points: &Vec<Point>) -> i32 {
+    if grid.get(coord) == Some('#') || grid.get(coord) == None {
+        return 0;
+    }
     let mut ans = 0;
-    if grid.end() == coord {
-        ans += 1;
-    }
-    for neighbor in grid.neighbors(coord) {
-        if past_points.contains(&neighbor) {
-            continue;
-        }
+    for neighbor in grid
+        .neighbors(coord)
+        .iter()
+        .filter(|point| !past_points.contains(point))
+    {
         let mut new_past_points = past_points.clone();
-        new_past_points.push(neighbor);
-        ans += dfs(grid, neighbor, past_points);
+        new_past_points.push(*neighbor);
+        ans = ans.max(dfs(grid, *neighbor, &new_past_points));
     }
-    return ans;
+    return ans + 1;
 }
 fn main() {
     let g: Vec<Vec<char>> = include_str!("../input/sample.txt")
@@ -77,6 +80,12 @@ fn main() {
         .map(|x| x.chars().into_iter().collect())
         .collect::<Vec<Vec<char>>>();
     let grid = Grid { grid: g };
-    let ans = dfs(&grid, grid.start(), &vec![]);
+    let ans = grid
+        .all_points()
+        .iter()
+        .map(|&point| dfs(&grid, point, &vec![point]))
+        .max()
+        .unwrap()
+        - 1;
     println!("{ans}");
 }
