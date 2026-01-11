01245e150a21680c0515a578eaa1907fba99722f
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-10 23:11:06 -0800

    cleanup

--- 2023/day23/part2/src/main.rs
+++ 2023/day23/part2/src/main.rs
@@ -8,7 +8,6 @@ struct Point {
 
 struct Graph {
     adj: HashMap<Point, Vec<(Point, usize)>>,
-    deg: HashMap<usize, Vec<Point>>, // maps degrees to list of nodes with that degree
 }
 
 trait HashMapVecExt<T, U> {
@@ -36,7 +35,6 @@ impl Graph {
     fn from(grid: Vec<Vec<char>>) -> Graph {
         let mut ans = Graph {
             adj: HashMap::new(),
-            deg: HashMap::new(),
         };
         const DIRECTIONS: [[i32; 2]; 4] = [[-1, 0], [0, 1], [1, 0], [0, -1]];
         for (x, row) in grid.iter().enumerate() {
@@ -51,7 +49,7 @@ impl Graph {
                         let p1 = Point { x, y };
                         let p2 = Point { x: new_x, y: new_y };
                         if p1 < p2 {
-                            ans.modify_edge(&p1, &p2, 1, true);
+                            ans.add_edge(p1, p2, 1);
                         }
                     }
                 }
@@ -59,45 +57,31 @@ impl Graph {
         }
         ans
     }
-    fn degree(&self, point: &Point) -> usize {
-        self.adj.get(point).map(|v| v.len()).unwrap_or(0)
+    fn add_edge(&mut self, p1: Point, p2: Point, weight: usize) {
+        self.adj.add_to_vec(p1, (p2, weight));
+        self.adj.add_to_vec(p2, (p1, weight));
     }
-    fn modify_edge(&mut self, point1: &Point, point2: &Point, weight: usize, to_add: bool) {
-        for point in [point1, point2] {
-            let d = self.degree(point);
-            self.deg.remove_from_vec(d, *point);
-        }
-        if to_add {
-            self.adj.add_to_vec(*point1, (*point2, weight));
-            self.adj.add_to_vec(*point2, (*point1, weight));
-        } else {
-            self.adj.remove_from_vec(*point1, (*point2, weight));
-            self.adj.remove_from_vec(*point2, (*point1, weight));
-        }
-        for point in [point1, point2] {
-            let d = self.degree(point);
-            self.deg.add_to_vec(d, *point);
-        }
+
+    fn remove_edge(&mut self, p1: Point, p2: Point, weight: usize) {
+        self.adj.remove_from_vec(p1, (p2, weight));
+        self.adj.remove_from_vec(p2, (p1, weight));
+    }
+
+    fn find_degree_2_node(&self) -> Option<Point> {
+        self.adj
+            .iter()
+            .find(|(_, neighbors)| neighbors.len() == 2)
+            .map(|(p, _)| *p)
     }
 
     fn compress(&mut self) {
-        // repeatedly find edges of degree 2 and remove them in favor of a single edge
-        while let Some(&node) = self.deg.get(&2).and_then(|v| v.first()) {
-            let neighbors: Vec<(Point, usize)> = self
-                .adj
-                .get(&node)
-                .unwrap()
-                .clone()
-                .into_iter()
-                .take(2)
-                .collect();
-            let weight1 = neighbors[0].1;
-            let weight2 = neighbors[1].1;
-            let node1 = neighbors[0].0;
-            let node2 = neighbors[1].0;
-            self.modify_edge(&node1, &node2, weight1 + weight2, true);
-            self.modify_edge(&node1, &node, weight1, false);
-            self.modify_edge(&node2, &node, weight2, false);
+        while let Some(node) = self.find_degree_2_node() {
+            let neighbors: Vec<(Point, usize)> = self.adj.remove(&node).unwrap();
+            let (node1, weight1) = neighbors[0];
+            let (node2, weight2) = neighbors[1];
+            self.remove_edge(node1, node, weight1);
+            self.remove_edge(node2, node, weight2);
+            self.add_edge(node1, node2, weight1 + weight2);
         }
     }
     fn dfs(&self, node: &Point, end: &Point, vis: &mut HashSet<Point>) -> Option<usize> {
