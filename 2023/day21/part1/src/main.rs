668f1b9171d51cd4baaf763d8ef0795551c81cfa
Maria Chrysafis <maria.chrysafis.junior@gmail.com>
2026-01-11 20:47:04 -0800

    cleanup

--- 2023/day21/part1/src/main.rs
+++ 2023/day21/part1/src/main.rs
@@ -1,15 +1,16 @@
-type Point = [i32; 2];
+type Point = [usize; 2];
 fn solve(grid: &Vec<Vec<char>>, point: Point, d: i32) -> Vec<Point> {
     if d == 1 {
         return [[-1, 0], [0, 1], [0, -1], [1, 0]]
             .iter()
-            .map(|[dx, dy]| [dx + point[0], dy + point[1]] as Point)
-            .filter(|point| {
-                point[0] >= 0
-                    && point[1] >= 0
-                    && point[0] < grid.len() as i32
-                    && point[1] < grid[0].len() as i32
-                    && grid[point[0] as usize][point[1] as usize] != '#'
+            .filter_map(|[dx, dy]| {
+                let x = point[0].checked_add_signed(*dx)?;
+                let y = point[1].checked_add_signed(*dy)?;
+                if x < grid.len() && y < grid[0].len() && grid[x][y] != '#' {
+                    Some([x, y])
+                } else {
+                    None
+                }
             })
             .collect();
     }
@@ -26,14 +27,13 @@ fn main() {
         .split("\n")
         .map(|str| str.chars().collect())
         .collect();
-    let mut point: Point = [0, 0];
-    for (i, row) in grid.iter().enumerate() {
-        for (j, c) in row.iter().enumerate() {
-            if *c == 'S' {
-                point = [i as i32, j as i32];
-            }
-        }
-    }
+    let point: Point = grid
+        .iter()
+        .enumerate()
+        .flat_map(|(i, row)| row.iter().enumerate().map(move |(j, &c)| (i, j, c)))
+        .find(|&(_, _, c)| c == 'S')
+        .map(|(i, j, _)| [i, j])
+        .unwrap();
     let ans = solve(&grid, point, 64);
     println!("{}", ans.len());
 }
